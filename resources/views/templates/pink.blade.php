9715a865c729cc1da80b1d2fec5151315efe1175
erickgato <itsbk0703@gmail.com>
2021-09-11 14:33:30 -0300

    feat(document): adiciona template base

--- resources/views/templates/pink.blade.php
+++ resources/views/templates/pink.blade.php
@@ -1,1 +1,8 @@
-Hello word
+@extends('templates.base-template')
+@push('styles')
+	<link rel="stylesheet" href="{{  mix('css/templates/pink.css') }}" >
+@endpush
+
+@section('template')
+	<span>Opa</span>
+@endsection
